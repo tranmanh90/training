d72dc44ebde2bfb826d8896f6fc7ce4be6ffe6ab
tranmanh90 <31216001+tranmanh90@users.noreply.github.com>
2018-07-30 10:53:06 +0700

    Add activity CallBack functions

--- Android002_Activity/app/src/main/java/com/training/activity/MainActivity.java
+++ Android002_Activity/app/src/main/java/com/training/activity/MainActivity.java
@@ -1,13 +1,47 @@
 package com.training.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG, "onCreate: Event");
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        Log.d(TAG, "onStart: Event");
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        Log.d(TAG, "onResume: Event");
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        Log.d(TAG, "onPause: Event");
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+        Log.d(TAG, "onStop: Event");
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        Log.d(TAG, "onDestroy: Event");
     }
 }
