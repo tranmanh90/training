87dea3608ab2bd35e66be75b370e18f5499d758c
tranmanh90 <31216001+tranmanh90@users.noreply.github.com>
2018-07-30 23:40:40 +0700

    Add click listener

--- Android003_StartActivityByActivity/app/src/main/java/com/training/activity/MainActivity.java
+++ Android003_StartActivityByActivity/app/src/main/java/com/training/activity/MainActivity.java
@@ -1,13 +1,35 @@
 package com.training.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+/*
+* Khởi chạy một Activity bằng một ACtivity khác thông qua phương thức:
+* startActivity() => Đưa người dùng tới màn hình tiếp theo.
+* startActivityForResult() => Khởi chạy một Activity sau đó nhận kết quả trả về.
+*
+* */
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mClickButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mClickButton = findViewById(R.id.button_click);
+        mClickButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this,SecondActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
