dd45b7f3ca26e93e541268408e0382ea6e771d12
tranmanh90 <31216001+tranmanh90@users.noreply.github.com>
2018-08-14 23:34:15 +0700

    Init Project

---
+++ App001_Weather/app/src/main/java/com/training/android/weather/WeatherActivity.java
@@ -0,0 +1,13 @@
+package com.training.android.weather;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class WeatherActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather);
+    }
+}
