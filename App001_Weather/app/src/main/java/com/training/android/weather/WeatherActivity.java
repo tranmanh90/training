828dd833613f9a2ddfd18537ee7c70de2e41f8fc
tranmanh90 <31216001+tranmanh90@users.noreply.github.com>
2018-08-15 17:30:35 +0700

    Mapping data to textView

--- App001_Weather/app/src/main/java/com/training/android/weather/WeatherActivity.java
+++ App001_Weather/app/src/main/java/com/training/android/weather/WeatherActivity.java
@@ -1,13 +1,50 @@
 package com.training.android.weather;
 
 import android.app.Activity;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.text.Html;
+import android.widget.TextView;
 
 public class WeatherActivity extends Activity {
+
+    private TextView cityField;
+    private TextView detailsField;
+    private TextView currentTemperatureField;
+    private TextView humidity_field;
+    private TextView pressure_field;
+    private TextView weatherIcon;
+    private TextView updatedField;
+
+    private Typeface weatherFont;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
+
+        weatherFont = Typeface.createFromAsset(getAssets(), "fonts/weathericons-regular-webfont.ttf");
+        cityField = (TextView) findViewById(R.id.city_field);
+        updatedField = (TextView) findViewById(R.id.updated_field);
+        detailsField = (TextView) findViewById(R.id.details_field);
+        currentTemperatureField = (TextView) findViewById(R.id.current_temperature_field);
+        humidity_field = (TextView) findViewById(R.id.humidity_field);
+        pressure_field = (TextView) findViewById(R.id.pressure_field);
+        weatherIcon = (TextView) findViewById(R.id.weather_icon);
+        weatherIcon.setTypeface(weatherFont);
+
+        Function.placeIdTask asynctask = new Function.placeIdTask(new Function.AsyncResponse() {
+            @Override
+            public void processFinish(String weather_city, String weather_description, String weather_temperature, String weather_humidity, String weather_pressure, String weather_updatedOn, String weather_iconText, String sun_rise) {
+                cityField.setText(weather_city);
+                updatedField.setText(weather_updatedOn);
+                detailsField.setText(weather_description);
+                currentTemperatureField.setText(weather_temperature);
+                humidity_field.setText("Humidity: " + weather_humidity);
+                pressure_field.setText("Pressure: " + weather_pressure);
+                weatherIcon.setText(Html.fromHtml(weather_iconText));
+            }
+        });
+        asynctask.execute("21.007852", "105.819877");
     }
 }
