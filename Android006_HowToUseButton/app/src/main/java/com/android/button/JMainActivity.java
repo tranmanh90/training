ae1d5245be5295c136859e825767bc20063f3445
tranmanh90 <31216001+tranmanh90@users.noreply.github.com>
2018-09-06 10:38:06 +0700

    Init project

---
+++ Android006_HowToUseButton/app/src/main/java/com/android/button/JMainActivity.java
@@ -0,0 +1,16 @@
+package com.android.button;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Tran Manh on 9/6/2018.
+ * Email: tranmanh.vn90@gmail.com
+ */
+public class JMainActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
