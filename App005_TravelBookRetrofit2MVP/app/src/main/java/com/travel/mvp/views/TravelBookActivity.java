d96b69e0d77858e9fce30b63fab0774efa394e54
tranmanh90 <31216001+tranmanh90@users.noreply.github.com>
2018-09-09 23:30:20 +0700

    Re-locate activity

--- App005_TravelBookRetrofit2MVP/app/src/main/java/com/travel/mvp/views/TravelBookActivity.java
+++ App005_TravelBookRetrofit2MVP/app/src/main/java/com/travel/mvp/views/TravelBookActivity.java
@@ -1,9 +1,11 @@
-package com.travel.mvp;
+package com.travel.mvp.views;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.travel.mvp.R;
+
+public class TravelBookActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
