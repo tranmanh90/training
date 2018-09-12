bec937afd5545482c93c4f478672a7c388bf6604
tranmanh90 <31216001+tranmanh90@users.noreply.github.com>
2018-09-12 13:32:10 +0700

    Add class RefreshToken

---
+++ App005_TravelBookRetrofit2MVP/app/src/main/java/com/travel/mvp/models/RefreshToken.java
@@ -0,0 +1,87 @@
+package com.travel.mvp.models;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by Tran Manh on 9/12/2018.
+ * Email: tranmanh.vn90@gmail.com
+ */
+public class RefreshToken {
+    @SerializedName("access_token")
+    private String accessToken;
+
+    @SerializedName("token_type")
+    private String tokenType;
+
+    @SerializedName("expires_in")
+    private Integer expiresIn;
+
+    @SerializedName("refresh_token")
+    private String refreshToken;
+
+    @SerializedName("created_at")
+    private String createdAt;
+
+    public RefreshToken() {
+    }
+
+    public RefreshToken(String accessToken, String tokenType, Integer expiresIn, String refreshToken, String createdAt) {
+        this.accessToken = accessToken;
+        this.tokenType = tokenType;
+        this.expiresIn = expiresIn;
+        this.refreshToken = refreshToken;
+        this.createdAt = createdAt;
+    }
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
+
+    public String getTokenType() {
+        return tokenType;
+    }
+
+    public void setTokenType(String tokenType) {
+        this.tokenType = tokenType;
+    }
+
+    public Integer getExpiresIn() {
+        return expiresIn;
+    }
+
+    public void setExpiresIn(Integer expiresIn) {
+        this.expiresIn = expiresIn;
+    }
+
+    public String getRefreshToken() {
+        return refreshToken;
+    }
+
+    public void setRefreshToken(String refreshToken) {
+        this.refreshToken = refreshToken;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuffer sb = new StringBuffer("RefreshToken{");
+        sb.append("accessToken='").append(accessToken).append('\'');
+        sb.append(", tokenType='").append(tokenType).append('\'');
+        sb.append(", expiresIn=").append(expiresIn);
+        sb.append(", refreshToken='").append(refreshToken).append('\'');
+        sb.append(", createdAt='").append(createdAt).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
+}
