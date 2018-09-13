406707111e6998c7e98e89ebdd1e4f98619c9cfa
tranmanh90 <31216001+tranmanh90@users.noreply.github.com>
2018-09-13 16:30:49 +0700

    Init project

---
+++ Android008_LoadMoreRecycleView/app/src/main/java/com/training/loadmore/LoadMoreActivity.java
@@ -0,0 +1,13 @@
+package com.training.loadmore;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoadMoreActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_load_more);
+    }
+}
